85e3a2ddad2e8950f63d2a88393e9e7650646820
Michael <mikeb@phase2online.com>
2024-12-05 01:17:18 -0600

    improve readability, probably, idk. someone tell me to go to sleep

--- node/day05/index.js
+++ node/day05/index.js
@@ -2,11 +2,17 @@ const fs = require('node:fs/promises')
 
 day05()
 
+/**
+ * Probably overdue that I setup an LSP and start using TypeScript 
+ * if I'm going to keep doing these in neovim...
+ */
+
 async function day05() {
     const data = await fs.readFile('day05.txt', { encoding: 'utf8' })
     const [ orderData, updateData ] = data.split('\n\n')
-    const orderMap = new Map()
+    const orderMap = new Map() // Maps a given page number to the list of page numbers that must come after it
 
+    // Fill the map
     orderData.split('\n').forEach(line => {
         const [key, value] = line.split('|')
         const laterPageNumbers = orderMap.get(key)
@@ -16,33 +22,10 @@ async function day05() {
         } else {
             orderMap.set(key, [value])
         }
-    })
-
-    const sumMiddlePageReducer = (sum, update) => {
-        return sum + parseInt(update[Math.floor(update.length / 2)])
-    }
-
-    const correctPageOrder = (pages) => {
-        let index = 0
-        while (index < pages.length) {
-            if (index === 0) {
-                index += 1
-            } else {
-                let previousPage = pages[index -1]
-                let currentPage = pages[index]
-                const laterPageNumbers = orderMap.get(currentPage)
-                if (laterPageNumbers && laterPageNumbers.includes(previousPage)) {
-                    pages[index - 1] = currentPage
-                    pages[index] = previousPage
-                    if (index > 1) index -= 1
-                } else index += 1
-            }
-        }
-        return pages 
-    }
+    })    
 
     const part1 = () => {
-        return updateData.split('\n').map(update => {
+        const orderedPages = updateData.split('\n').map(update => {
             if (update.length === 0) return
             const pages = update.split(',')
             const isCorrectUpdate = pages.every((page, index, arr) => {
@@ -53,12 +36,31 @@ async function day05() {
             })
             if (isCorrectUpdate) return pages
         })
-        .filter(result => (result))
-        .reduce(sumMiddlePageReducer, 0)
+
+        return sumMiddlePages(orderedPages)
     }
 
     const part2 = () => {
-        return updateData.split('\n').map(update => {
+        const correctPageOrder = (pages) => {
+            let index = 0
+            while (index < pages.length) {
+                if (index === 0) {
+                    index += 1
+                } else {
+                    let previousPage = pages[index -1]
+                    let currentPage = pages[index]
+                    const laterPageNumbers = orderMap.get(currentPage)
+                    if (laterPageNumbers && laterPageNumbers.includes(previousPage)) {
+                        pages[index - 1] = currentPage
+                        pages[index] = previousPage
+                        if (index > 1) index -= 1
+                    } else index += 1
+                }
+            }
+            return pages 
+        }
+
+        const reorderedPages = updateData.split('\n').map(update => {
             if (update.length === 0) return
             const pages = update.split(',')
             const isIncorrectUpdate = pages.some((page, index, arr) => {
@@ -69,9 +71,12 @@ async function day05() {
             })
             if (isIncorrectUpdate) return correctPageOrder(pages)
         })
-        .filter(result => (result))
-        .reduce(sumMiddlePageReducer, 0)
+        
+        return sumMiddlePages(reorderedPages)
     }
+
+    const sumMiddlePages = (pages) => pages.filter(result => (result))
+    .reduce((sum, update) => sum + parseInt(update[Math.floor(update.length / 2)]), 0)
 
     console.log(`Part 1: ${part1()}`)
     console.log(`Part 2: ${part2()}`)
