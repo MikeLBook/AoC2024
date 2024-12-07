c5adfa4b65a777924e9c03c3d81e164980767654
Michael <mikeb@phase2online.com>
2024-12-06 23:06:24 -0600

    thank god

--- node/day06/index.js
+++ node/day06/index.js
@@ -1,5 +1,7 @@
 /**
- * Solution assumes all inputs start with the guard facing up.
+ * I love this problem but hate this solution. 
+ * I'll need to revisit this one.
+ * The run time on part 2 is atrocious.
  */
 
 const fs = require('node:fs/promises')
@@ -16,28 +18,37 @@ const directions = {
 async function day06() {
     const data = await fs.readFile('day06.txt', { encoding: 'utf8' })
     const grid = data.split('\n').map(line => line.split('')).filter(row => row.length > 0 )
-    const occupiedPositions = []
+
+    let occupiedPositions = []
+    let startingPosition = undefined
+    let position = undefined
     let direction = directions.up
-    let position
+    let obstruction = undefined
 
     // find starting position
     let y = 0
     while (!position) {
         const row = grid[y]
         const x = row.findIndex(symbol => symbol === '^')
-        if (x !== -1) position = { x, y }
-        else y++
+        if (x !== -1) {
+            position = { x, y }
+            startingPosition = { x, y }
+        } else y++
     }
 
     const getNextPosition = () => {
         return {x: position.x + direction.x, y: position.y + direction.y}
     }
 
-    const isValidPosition = (coordinates) => {
+    const isValidGridPosition = (coordinates) => {
         return coordinates.x >= 0
         && coordinates.x < grid.length
         && coordinates.y >= 0
         && coordinates.y < grid.length
+    }
+
+    const isObstructedPosition = (coordinates) => {
+        return grid[coordinates.y][coordinates.x] === '#' || JSON.stringify(coordinates) === JSON.stringify(obstruction)
     }
 
     const changeDirection = () => {
@@ -57,25 +68,54 @@ async function day06() {
         }
     }
 
-    const part1 = () => {
+    const hasDejaVu = () => {
+        return occupiedPositions.includes(JSON.stringify([position, direction]))
+    }
+
+    const walkPatrolRoute = () => {
         while(position) {
+            if (hasDejaVu()) break
             const nextPosition = getNextPosition()
-            if (isValidPosition(nextPosition)) {
-                const nextCharacter = grid[nextPosition.y][nextPosition.x]
-                if (nextCharacter !== '#') {
-                    occupiedPositions.push(position)
+            if (isValidGridPosition(nextPosition)) {
+                if (!isObstructedPosition(nextPosition)) {
+                    occupiedPositions.push(JSON.stringify([position, direction]))
                     position = nextPosition
                 } else {
                     changeDirection()
                 }
             } else {
-                occupiedPositions.push(position)
+                occupiedPositions.push(JSON.stringify([position, direction]))
                 position = undefined
             }
         }
-        const positionSet = new Set(occupiedPositions.map(coord => JSON.stringify(coord)))
+    }
+
+    const part1 = () => {
+        walkPatrolRoute()
+        const positionSet = new Set(occupiedPositions.map(pos => JSON.stringify(JSON.parse(pos)[0])))
         return positionSet.size
     }
 
+    const part2 = () => {
+        let successfulObstructions = 0
+        for (let y = 0; y < grid.length; y++) {
+            for (let x = 0; x < grid.length; x++) {
+                if (!(grid[y][x] === '#')) {
+                    occupiedPositions = []
+                    position = startingPosition
+                    direction = directions.up
+                    obstruction = {x, y}
+                    console.log(obstruction)
+                    walkPatrolRoute()
+                    if (position) {
+                        successfulObstructions++
+                    }
+                }
+            }
+        }
+        return successfulObstructions
+    }
+
     console.log(`Part 1: ${part1()}`)
+    console.log(`Part 2: ${part2()}`)
 }
