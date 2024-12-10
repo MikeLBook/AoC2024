c90883cee673e68b7fe61d8068e4fa1077237a5e
Michael <mikeb@phase2online.com>
2024-12-09 22:24:31 -0600

    day 9 part 1

--- kotlin/AoC2024Kotlin/src/day09/Day09.kt
+++ kotlin/AoC2024Kotlin/src/day09/Day09.kt
@@ -3,54 +3,45 @@ package day09
 import day.Day
 
 class Day09: Day("src/day09/input.txt") {
-    val disk = input.readText()
-
-    override fun doPart1(): Long {
-        var fileBlockIndex = 0
-        var freeBlockCount = 0
-        var blockString = ""
-
+    private val disk = input.readText()
+    private var blocks: MutableList<String> = buildList {
+        var blockID = 0
         disk.forEachIndexed { index, char ->
             if (index % 2 == 0) {
                 for (i in 1..char.digitToInt()) {
-                    blockString = "$blockString$fileBlockIndex"
+                    add(blockID.toString())
                 }
-                fileBlockIndex++
+                blockID++
             } else {
                 for (i in 1..char.digitToInt()) {
-                    blockString = "$blockString."
-                    freeBlockCount++
+                    add(".")
                 }
             }
         }
+    }.toMutableList()
 
-        val blockList = blockString
-            .split("")
-            .filter { it.isNotBlank() }
-            .toMutableList()
-
-        val freeBlockIndices = blockList.foldIndexed(mutableListOf<Int>()) { index, acc, _ ->
-            val block = blockList[index]
+    override fun doPart1(): Long {
+        val freeBlockIndices = blocks.foldIndexed(mutableListOf<Int>()) { index, acc, _ ->
+            val block = blocks[index]
             if (block == ".") acc.add(index)
             acc
         }
 
-        for (i in blockList.indices.reversed()) {
-            val block = blockList[i]
+        for (i in blocks.indices.reversed()) {
+            val block = blocks[i]
             if (block != ".") {
                 val emptyBlockIndex = freeBlockIndices.removeFirstOrNull()
                 if (emptyBlockIndex != null && emptyBlockIndex < i) {
-                    blockList[emptyBlockIndex] = block
-                    blockList[i] = "."
+                    blocks[emptyBlockIndex] = block
+                    blocks[i] = "."
                 }
             }
         }
 
-        return blockList.foldIndexed(0L) { index, checksum, id ->
+        return blocks.foldIndexed(0L) { index, checksum, id ->
             if (id != ".") checksum + (index.toLong() * id.toLong()) else checksum
         }
     }
-
 
     override fun doPart2(): Any {
         TODO("Not yet implemented")
