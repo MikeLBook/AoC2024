7b25bc312ccc46fa070dca5b646a7e37e023627d
Michael <mikeb@phase2online.com>
2024-12-09 19:32:23 -0600

    Not sure what I'm missing yet

--- kotlin/AoC2024Kotlin/src/day09/Day09.kt
+++ kotlin/AoC2024Kotlin/src/day09/Day09.kt
@@ -2,27 +2,55 @@ package day09
 
 import day.Day
 
-class Day09: Day("src/day09/sample.txt") {
+class Day09: Day("src/day09/input.txt") {
     val disk = input.readText()
 
-    override fun doPart1(): Any {
+    override fun doPart1(): Long {
         var fileBlockIndex = 0
-        var blocks = ""
+        var freeBlockCount = 0
+        var blockString = ""
+
         disk.forEachIndexed { index, char ->
             if (index % 2 == 0) {
                 for (i in 1..char.digitToInt()) {
-                    blocks = "$blocks$fileBlockIndex"
+                    blockString = "$blockString$fileBlockIndex"
                 }
                 fileBlockIndex++
             } else {
                 for (i in 1..char.digitToInt()) {
-                    blocks = "$blocks."
+                    blockString = "$blockString."
+                    freeBlockCount++
                 }
             }
         }
-        
-        return blocks
+
+        val blockList = blockString
+            .split("")
+            .filter { it.isNotBlank() }
+            .toMutableList()
+
+        val freeBlockIndices = blockList.foldIndexed(mutableListOf<Int>()) { index, acc, _ ->
+            val block = blockList[index]
+            if (block == ".") acc.add(index)
+            acc
+        }
+
+        for (i in blockList.indices.reversed()) {
+            val block = blockList[i]
+            if (block != ".") {
+                val emptyBlockIndex = freeBlockIndices.removeFirstOrNull()
+                if (emptyBlockIndex != null && emptyBlockIndex < i) {
+                    blockList[emptyBlockIndex] = block
+                    blockList[i] = "."
+                }
+            }
+        }
+
+        return blockList.foldIndexed(0L) { index, checksum, id ->
+            if (id != ".") checksum + (index.toLong() * id.toLong()) else checksum
+        }
     }
+
 
     override fun doPart2(): Any {
         TODO("Not yet implemented")
@@ -32,5 +60,5 @@ class Day09: Day("src/day09/sample.txt") {
 fun main() {
     val day = Day09()
     day.part1()
-    day.part2()
+//    day.part2()
 }
