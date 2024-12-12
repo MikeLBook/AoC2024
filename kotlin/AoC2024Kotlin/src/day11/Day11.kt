7773586ee2fb6b6eac7062cdfb488c827d48793c
Michael <mikeb@phase2online.com>
2024-12-12 00:07:17 -0600

    <3

--- kotlin/AoC2024Kotlin/src/day11/Day11.kt
+++ kotlin/AoC2024Kotlin/src/day11/Day11.kt
@@ -5,6 +5,7 @@ import models.Day
 class Day11: Day("src/day11/input.txt") {
     private val initialArrangement = input.readText().split(" ")
 
+    // It's tempting to refactor part 1 to use part 2's solution, but I'll keep it around.
     override fun doPart1(): Int {
         var stones = initialArrangement
         for (i in 1..25) {
@@ -28,25 +29,25 @@ class Day11: Day("src/day11/input.txt") {
 
     override fun doPart2(): Long {
         return initialArrangement.sumOf { startingStone ->
-            var stoneCount = 1L
-            fun blink(stone: String, blinks: Int) {
-                if (blinks <= 75) {
-                    when {
-                        stone == "0" -> blink("1", blinks + 1)
-                        stone.length % 2 == 0 -> {
-                            blink(stone.substring(0, stone.length / 2).toLong().toString(), blinks + 1)
-                            blink(stone.substring(stone.length /2).toLong().toString(), blinks + 1)
-                            stoneCount++
-                        }
-                        else -> {
-                            val product = stone.toLong() * 2024
-                            blink(product.toString(), blinks + 1)
-                        }
-                    }
+            1 + blink(startingStone, 1, mutableMapOf())
+        }
+    }
+
+    private fun blink(stone: String, blinks: Int, cache: MutableMap<Pair<String, Int>, Long>): Long {
+        if (blinks > 75) return 0
+        return cache.getOrPut(stone to blinks) {
+            when {
+                stone == "0" -> blink("1", blinks + 1, cache)
+                stone.length % 2 == 0 -> {
+                    val splitStoneA = stone.substring(0, stone.length / 2).toLong().toString()
+                    val splitStoneB = stone.substring(stone.length / 2).toLong().toString()
+                    1 + blink(splitStoneA, blinks + 1, cache) + blink(splitStoneB, blinks + 1, cache)
+                }
+                else -> {
+                    val product = stone.toLong() * 2024
+                    blink(product.toString(), blinks + 1, cache)
                 }
             }
-            blink(startingStone, 1)
-            stoneCount
         }
     }
 }
